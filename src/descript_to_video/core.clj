7e6b36e8e41fe0dc5082418c97473516b59e3594
simana_sch <tktossi@live.com>
2021-06-14 23:39:47 +0900

    とりあえずファイル出力できるようにした

--- src/descript_to_video/core.clj
+++ src/descript_to_video/core.clj
@@ -1,6 +1,7 @@
 (ns descript-to-video.core
-  (:require [clojure.string :only (join)])
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [clojure.string :only join]))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -19,3 +20,23 @@
   (let [encoded (hexify text)
         pad (take (- 4096 (count encoded)) (repeat "0"))]
     (apply str encoded pad)))
+
+(defn get-template
+  "paramに対応するaviUtlのオブジェクトのファイル名を返す
+   TODO:ファイル名を設定ファイルとかディレクトリ内のalias名から取得するようにする"
+  [template]
+  (cond (= template "hoge") "fuga"
+        :else "resources/alias/default.exo"))
+
+(defn set-object-body
+  "TODO:Mapを使った実装にする?"
+  [body key seq]
+  (map #(if (= %1 key) (str %1 (format-text-as-aviutl-object body)) (str %1)) seq))
+
+(defn gen-object
+  "templateをコピーし、表示するテキストがtextのオブジェクトを生成する
+   TODO:ファイル名を変数にする、処理をきれいにする、set-object-bodyあたりを分割する"
+  [text template & rest]
+  (with-open [r (io/reader (get-template template) :encoding "shift-jis")
+              o (io/writer "test.exo" :encoding "shift-jis")]
+    (.write o (clojure.string/join "\r\n" (reduce conj [] (set-object-body text "text=" (line-seq r)))))))
