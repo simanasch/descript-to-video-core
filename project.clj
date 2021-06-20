4953875f10b595871d8a80d23c17fe685e971e37
simana_sch <tktossi@live.com>
2021-06-20 14:14:00 +0900

    clojureCLR用の設定を追加

--- project.clj
+++ project.clj
@@ -1,11 +1,29 @@
 (defproject descript-to-video "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-clr "0.2.2"]]
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [instaparse "1.4.10"]]
   :main ^:skip-aot descript-to-video.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :profiles {
+            ;;  :uberjar {:aot :all
+            ;;            :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :clr {:aot :all}}
+  :clr {:cmd-templates  {:clj-exe   [[?PATH "mono"] [CLJCLR %1]]
+                         :clj-dep   [[?PATH "mono"] ["target/clr/clj/Debug 4.0" %1]]
+                         :clj-url   "http://sourceforge.net/projects/clojureclr/files/clojure-clr-1.4.1-Debug-4.0.zip/download"
+                         :clj-zip   "clojure-clr-1.4.1-Debug-4.0.zip"
+                         :curl      ["curl" "--insecure" "-f" "-L" "-o" %1 %2]
+                         :nuget-ver [[?PATH "mono"] [*PATH "nuget.exe"] "install" %1 "-Version" %2]
+                         :nuget-any [[?PATH "mono"] [*PATH "nuget.exe"] "install" %1]
+                         :unzip     ["unzip" "-d" %1 %2]
+                         :wget      ["wget" "--no-check-certificate" "--no-clobber" "-O" %1 %2]}
+        :deps-cmds      [[:curl  :clj-zip :clj-url]
+                         [:unzip "../clj" :clj-zip]
+                         ]
+        :main-cmd      [:clj-exe "Clojure.Main.exe"]
+        :compile-cmd   [:clj-exe "Clojure.Compile.exe"]})
