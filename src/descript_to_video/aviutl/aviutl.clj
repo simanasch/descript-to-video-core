c4422ec72729f45c9c459226576dac4336a5826a
simana_sch <tktossi@live.com>
2021-07-11 23:45:19 +0900

    aviutlのobjectの文字列->stringの変換追加

--- src/descript_to_video/aviutl/aviutl.clj
+++ src/descript_to_video/aviutl/aviutl.clj
@@ -5,14 +5,6 @@
             [descript-to-video.util.format :as formatter]
             [instaparse.core :as insta]))
 
-(defn format-text-as-aviutl-object
-  "引数のテキストをaviUtl拡張編集の.obj形式のテキストにする
-   テキストをUTF-16LEでエンコードした場合のbinary値を、lengthが4096になるまで右側に0詰めする"
-  [text]
-  (let [encoded (formatter/hexify text)
-        pad (take (- 4096 (count encoded)) (repeat "0"))]
-    (apply str encoded pad)))
-
 (defn get-template
   "paramに対応するaviUtlのオブジェクトのファイル名を返す
    TODO:ファイル名を設定ファイルとかディレクトリ内のalias名から取得するようにする"
@@ -24,7 +16,7 @@
   "TODO:Mapを使った実装にする?"
   [body key seq]
   (map
-   #(if (= %1 key) (str %1 (format-text-as-aviutl-object body)) %1) ;;
+   #(if (= %1 key) (str %1 (formatter/string->padded-hex body)) %1) ;;
    seq))
 
 (defn gen-object
