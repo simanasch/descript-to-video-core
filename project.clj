eb40ca486183771f891f39e5d16ba418ebebe3bb
simana_sch <tktossi@live.com>
2021-08-08 23:45:18 +0900

    設定ファイルをyamlに変更

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [io.forward/yaml "1.0.10"]
                  [instaparse "1.4.10"]]
   :main ^:skip-aot descript-to-video.core
   :target-path "target/%s"
