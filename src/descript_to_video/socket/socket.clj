cd4bb8576030291a341481d7e0d29b7e5623e562
simana_sch <tktossi@live.com>
2021-08-08 09:34:08 +0900

    名前つきパイプの処理整理

--- src/descript_to_video/socket/socket.clj
+++ src/descript_to_video/socket/socket.clj
@@ -1,18 +1,46 @@
 (ns descript-to-video.socket.socket
   (:require [clojure.java.io :as io]
-            [clojure.string :as s]))
+            [clojure.string :as s])
+  (:import [java.io RandomAccessFile]))
 
-(def writer (atom (io/writer "//./pipe/testpipe") ))
-;; (def reader (atom (io/reader "//./pipe/testpipe") ))
-;; (.read @reader)
-(.write @writer "おはようございます\n")
-(.flush @writer)
-(.write @writer "葵ちゃんだよ\n")
-(.flush @writer)
-(.write @writer "これはサンプルのテキストです\n")
-(.close @writer)
-;; (.write @writer "end")
-;; (.newLine @writer)
+(defn write-to-pipe
+  [^RandomAccessFile pipe ^String text]
+  (do
+   (.write pipe (.getBytes text))
+   (.readLine pipe)))
+
+(comment
+  (def pipe (atom (io/file "//./pipe/testpipe" )))
+  (def pipe (atom (RandomAccessFile. "//./pipe/testpipe" "rw")))
+  (def writer (atom (io/writer "//./pipe/testpipe")))
+
+  (def reader (atom (io/reader "//./pipe/testpipe")))
+
+  (.read @reader)
+  (.readLine @pipe)
+
+  (.write @writer "おはようございます\n")
+  (.write @pipe (.getBytes "おはようございます\n"))
+  (write-to-pipe @pipe "おはようございます")
+
+  (.flush @writer)
+
+  (.write @writer "葵ちゃんだよ\n")
+  (.write @pipe (.getBytes "葵ちゃんだよ\n"))
+
+  (.flush @writer)
+
+  (.write @writer "これはサンプルのテキストです\n")
+
+  (.close @writer)
+  (.close @pipe)
+
+  (.write @writer "end")
+
+  (.newLine @writer)
+  )
+
+
 
 ;; (with-open [rdr (io/reader "//./pipe/testpipe") ]
 ;;    (printf "%s\n" (s/join "\n" (line-seq rdr))))
