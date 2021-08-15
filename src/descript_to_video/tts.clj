07fe7f20d2797bc6b927ca7d8a941dfac7af36fb
simana_sch <tktossi@live.com>
2021-08-15 20:58:30 +0900

    とりあえずTTSの出力ができるようにした

--- src/descript_to_video/tts.clj
+++ src/descript_to_video/tts.clj
@@ -1,25 +1,14 @@
 (ns descript-to-video.tts
   (:require [clojure.java.shell :as shell]
-            [clojure.string :as s]))
+            [clojure.string :as s]
+            [clojure.java.io :as io]))
 
+;; 定数群
+;; TODO:設定ファイルから取得
 (def ttsControllerPath "%USERPROFILE%\\TTSController\\src\\SpeechSample\\bin\\x64\\Release\\SpeechSample.exe")
-
 (def separator #"-----\r\n")
+(def default-output-path "output/voices/")
 
-(def default-output-path "voices/")
-
-(defn talk
-  "TTSライブラリ名とテキストを指定し、SpeechSample.exeで喋らせる"
-  [library text]
-  (shell/sh "cmd" "/c" ttsControllerPath "-t" text "-n" library))
-
-(defn gen-wav-name
-  [library text]
-  (str library "_" text ".wav"))
-
-(defn save-to-file
-  [library text]
-  (shell/sh "cmd" "/c" ttsControllerPath "-t" text "-n" library "-o" (str default-output-path (gen-wav-name library text))))
 
 (defn get-library-list
   "SpeechSample.exeを実行し、結果から使用可能なTTSライブラリの一覧を取得する"
@@ -31,12 +20,29 @@
       (= exitCode 0) (s/split-lines output)
       :else [])))
 
+(defn talk
+  "TTSライブラリ名とテキストを指定し、SpeechSample.exeで喋らせる"
+  [library text]
+  (shell/sh "cmd" "/c" ttsControllerPath "-t" (str text) "-n" library))
+
+(defn gen-wav-name
+  [library text]
+  (str library "_" text ".wav"))
+
+(defn save-to-file
+  [library text]
+  (let [outFileName (str default-output-path (gen-wav-name library text))]
+    (io/make-parents outFileName)
+    (shell/sh "cmd" "/c" ttsControllerPath "-t" (str text) "-n" library "-o" outFileName)))
+
+
+
 (comment
-  (def libraries (get-library-list))
-  libraries
   (talk "さとうささら" "さとうささらです")
   
   (talk "葵" "さとうささらです")
-  (save-to-file "葵" "さとうささらです")
+  (save-to-file "葵" "葵です")
+  (save-to-file "茜" "あかねちゃんやで")
+
 
   )
