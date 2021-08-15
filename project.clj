1b46222428f30e654ce24845524936ba8b66d975
simana_sch <tktossi@live.com>
2021-08-15 20:36:06 +0900

    asyncライブラリを追加

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [io.forward/yaml "1.0.10"]
-                ;;  [com.vladsch.flexmark/flexmark "0.62.2"]
+                 [org.clojure/core.async "1.3.618"]
                  [com.vladsch.flexmark/flexmark-all "0.62.2"]
                  [instaparse "1.4.10"]]
   ;; :repositories [["com.vladsch.flexmark/flexmark-java" "https://github.com/vsch/flexmark-java"]]
