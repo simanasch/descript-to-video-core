1a736822be909f3ee9d4731fcf196c9538c451e7
simana_sch <tktossi@live.com>
2021-08-19 21:37:55 +0900

    とりあえずコンパイル通るようにした

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [io.forward/yaml "1.0.10"]
                  [org.clojure/core.async "1.3.618"]
-                 [com.vladsch.flexmark/flexmark-all "0.62.2"]
+                ;;  [com.vladsch.flexmark/flexmark-all "0.62.2"]
                  [instaparse "1.4.10"]]
   ;; :repositories [["com.vladsch.flexmark/flexmark-java" "https://github.com/vsch/flexmark-java"]]
   :main ^:skip-aot descript-to-video.core
--- src/descript_to_video/aviutl/parser.clj
+++ src/descript_to_video/aviutl/parser.clj
@@ -1,20 +1,9 @@
 (ns descript-to-video.aviutl.parser
   (:require [instaparse.core :as insta]))
 
-(def aviutl-exobject-format
-  (insta/parser 
-   "exo=project layer+
-    project = \"\[exedit\]\"CRLF
-    CRLF=\r\n"))
-
-(comment
-  
-  (def raw-project-file (slurp "E:/Documents/descript-to-video/output/template.exo" :encoding "shift-jis"))
-  (def objects (clojure.string/split raw-project-file #"\]\r\n"))
-  (nth objects 3)
-  (def object-parser
-    (insta/parser
-     "S = exedit object+ 
+(def object-parser
+  (insta/parser
+   "S = exedit object+ 
       exedit = #'\\[exedit\\]\r\n' property+
       property = name <'='> value  <CRLF>
       name = #'[^\\[=]+'
@@ -25,6 +14,12 @@
       layer = #'\\d+'
       order = #'\\d+'
       "))
+
+(comment
+  (def raw-project-file (slurp "E:/Documents/descript-to-video/output/template.exo" :encoding "shift-jis"))
+  (def objects (clojure.string/split raw-project-file #"\]\r\n"))
+  (nth objects 3)
+
   (def raw-object (slurp "./resources/alias/default.exo" :encoding "shift-jis"))
   (def parsed-object (object-parser raw-object))
   (clojure.pprint/pprint parsed-object)
