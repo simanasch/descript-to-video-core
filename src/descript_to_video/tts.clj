e0b26c3e1d7667bc1ec77248c6c0c8ccfafcd15c
simana_sch <tktossi@live.com>
2021-08-22 15:23:52 +0900

    とりあえずmain関数を呼ぶと音声が保存できるようにした

--- src/descript_to_video/tts.clj
+++ src/descript_to_video/tts.clj
@@ -6,7 +6,6 @@
 ;; 定数群
 ;; TODO:設定ファイルから取得
 (def ttsControllerPath "%USERPROFILE%\\TTSController\\src\\SpeechSample\\bin\\x64\\Release\\SpeechSample.exe")
-;; (def ttsControllerPath "%USERPROFILE%\\TTSController\\src\\SpeechSample\\bin\\x64\\Debug\\SpeechSample.exe")
 (def separator #"-----\r\n")
 (def default-output-path "output/voices/")
 
@@ -26,6 +25,11 @@
   [library text]
   (shell/sh "cmd" "/c" ttsControllerPath "-t" (str text) "-n" library))
 
+(defn talk-agent
+  [library text]
+  (let [ag (agent library)]
+    (send-off ag #(talk %1 %2) text)))
+
 (defn gen-wav-name
   [library text]
   (str library "_" text ".wav"))
@@ -33,16 +37,27 @@
 (defn save-to-file
   [library text]
   (let [joinedText (cond (seq? text) (s/join "。" text) :else text)
-        outFileName (str default-output-path (gen-wav-name library joinedText))]
+        outFileName (str default-output-path (gen-wav-name library joinedText)) ]
     (io/make-parents outFileName)
-    ;; (println outFileName)
+    (println outFileName)
     (shell/sh "cmd" "/c" ttsControllerPath "-t" joinedText "-n" library "-o" outFileName)))
 
-
+(defn save-to-file-agent
+  [library text]
+  (let [ag (agent library)]
+    (send-off ag #(save-to-file %1 %2) text)))
 
 (comment
-  (get-library-list)
-  (talk "さとうささら" "さとうささらです")
+  (def sample-texts '(["ゆかり" "これはサンプルのスライドです"] ["ゆかり" "解説はコメントに書くようにしています、"] ["ゆかり" "テキストを書いてコマンド実行すると、"] ["ゆかり" "コメントの中からvoiceroidで読み上げるテキストを拾って"] ["ゆかり" "読み上げが終わったら次のスライドを表示するようにしてくれます"] ["ゆかり" "biim風のレイアウトにもできます"]))
+  (def talk-task (talk-agent "葵" "さとうささらです"))
+  (def ag (agent "葵"))
+  (send-off ag #(talk %1 %2)  "さとうささらです")
+  (await-for 10000 ag)
+  (await-for 10000 talk-task)
+  (let [ag (talk-agent "茜" "茜ちゃんやで")]
+    (await-for 10000 ag))
+  (let [ag (save-to-file-agent "茜" "茜ちゃんやで")]
+    (await-for 10000 ag))
 
   (talk "葵" "さとうささらです")
   (save-to-file "葵" "葵です")
