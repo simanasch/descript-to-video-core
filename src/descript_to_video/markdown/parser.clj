5bfd9fe8ce0100d8dd6c042caeb4f8a72ab9c024
simana_sch <tktossi@live.com>
2021-08-22 23:34:57 +0900

    parserの処理をシンプルに

--- src/descript_to_video/markdown/parser.clj
+++ src/descript_to_video/markdown/parser.clj
@@ -5,18 +5,17 @@
 (def separator "＞")
 
 (defn get-voiceroid-text-lines
+  "引数のテキストから、Separatorを含む行をSeparator前後でsplitしたlistを返す"
   [text]
-  (map 
-   #(s/split (s/trim %) (re-pattern separator)) 
-   (filter 
-    #(s/includes? % separator) 
-    (s/split-lines text))))
+  (let [lines (s/split-lines text)
+        has-separator-line (filter #(s/includes? % separator) lines)]
+    (map #(s/split (s/trim %) (re-pattern separator)) has-separator-line)))
 
 (defn get-body-text-lines
+  "引数のテキストのうち、Separatorを含まない行を返す"
   [text]
-  (filter 
-   #(not (s/includes? % separator))
-   (s/split-lines text)))
+  (let [lines (s/split-lines text)]
+    (filter #(not (s/includes? % separator)) lines)))
 
 (comment
   
