1bfaeca5643b247b451e6584a8f1078d936c4716
simana_sch <tktossi@live.com>
2021-09-29 22:40:30 +0900

    ttsのwavファイル、テキスト内容からaviutlのオブジェクト作成をする処理を追加

--- src/descript_to_video/aviutl/aviutl.clj
+++ src/descript_to_video/aviutl/aviutl.clj
@@ -4,6 +4,7 @@
             [clojure.walk :as w]
             [descript-to-video.util.format :as formatter]
             [descript-to-video.util.map :as m]
+            [descript-to-video.util.audio :as a]
             [descript-to-video.aviutl.parser :as parser]
             [yaml.core :as yaml])
   (:refer  flatland.ordered.map))
@@ -13,7 +14,7 @@
 
 (defn- get-settings
   []
-  (yaml/from-file default-settings-path))
+ (yaml/from-file default-settings-path))
 
 (def prefix "<?s=[==[\r\n")
 (def suffix "\r\n]==];require(\"PSDToolKit\").prep.init({ls_mgl=0,ls_mgr=0,st_mgl=0,st_mgr=0,sl_mgl=0,sl_mgr=0,},obj,s)?>")
@@ -60,10 +61,25 @@
   [path start end]
   (copy-from-object-as-ordered-map
    (map->ordered-map
-    {:0 {:start start
-         :end end
+    {:0 {:start (str start)
+         :end (str end)
          :0.0 {:file path}}})
    "slide"))
+
+(defn get-tts-object
+  [start path text library]
+  (let [end (dec (+ start (a/get-wav-length path)))
+        absolute-path (.getAbsolutePath (io/file path))]
+    ;; (println "raw text:" text)
+    (copy-from-object-as-ordered-map
+     (map->ordered-map
+      {:0 {:start (str start)
+           :end (str end)
+           :0.0 {:text (formatter/string->padded-hex (str prefix text suffix))}}
+       :1 {:start (str start)
+           :end (str end)
+           :1.0 {:file absolute-path}}})
+     library)))
 
 ;; 動作確認用のスクリプト類
 ;; TODO:テストに移動
@@ -73,17 +89,18 @@
   (gen-object "ゆかりさんです" "ゆかり")
   ;; (parser/aviutl-object->yaml)
   (def slide-replace-map '{:0 {:start "0" :end "300" :0.0 {:file "E:\\Videos\\VoiceroidWaveFiles.002.png"}}})
-  (:start (:0  replaced))
-  (:0.0 (:0 (copy-from-object-as-ordered-map {} "slide")))
   (def raw-slide-template (parser/aviutl-object->yaml (slurp (get-template "slide") :encoding "shift-jis")))
-  (def update-map (ordered-map {:0 {:start "300" :end "600" :0.0 (ordered-map {:file "E:\\Videos\\VoiceroidWaveFiles.002.png"})}}))
-  (apply into (ordered-map) '{:0 {:start "300" :end "600" :0.0 {:file "E:\\Videos\\VoiceroidWaveFiles.002.png"}}})
+  ;; slideの.exoで更新する内容の期待値
+  (def slide-update-map (ordered-map {:0 {:start "300" :end "600" :0.0 (ordered-map {:file "E:\\Videos\\VoiceroidWaveFiles.002.png"})}}))
   (println (parser/yaml->aviutl-object raw-slide-template))
   (get-slide-object-as-ordered-map "E:\\Videos\\VoiceroidWaveFiles.002.png"  "300" "600")
-  (println (parser/yaml->aviutl-object (get-slide-object-as-ordered-map "E:\\Videos\\VoiceroidWaveFiles.002.png"  "300" "600")))
+  (spit "output/slide2.exo" (parser/yaml->aviutl-object (get-slide-object-as-ordered-map "E:\\Videos\\VoiceroidWaveFiles.002.png"  "300" "600")) :encoding "shift-jis")
   (m/deep-merge-with (fn [v1 v2] (println v1 v2) (cond (map? v2) (into (ordered-map) v2) :else v1)) '{:0 {:start "300" :end "600" :0.0 {:file "E:\\Videos\\VoiceroidWaveFiles.002.png"}}})
   (ordered-map slide-replace-map)
-  (clojure.walk/postwalk-demo slide-replace-map)
-  (def obj-postwalk (clojure.walk/postwalk #(cond (map? %) (ordered-map %) :else %) slide-replace-map))
-  (get-in (ordered-map {:0 {:start "0" :end "300" :0.0 (ordered-map {:file "E:\\Videos\\VoiceroidWaveFiles.002.png"})}}) '[:0 :0.0 :file])
+  (def sample-wav (io/file "./output/voices/210923210356664_ゆかり_おっつおっつ.wav"))
+  (.getAbsolutePath sample-wav)
+  (parser/aviutl-object->yaml (slurp (get-template "ゆかり") :encoding "shift-jis"))
+  (def vo (get-voice-object 20 (.getAbsolutePath sample-wav) (slurp "./output/voices/210923210356664_ゆかり_おっつおっつ.txt" :encoding "shift-jis") "ゆかり"))
+  (println (parser/yaml->aviutl-object vo))
+  (formatter/padded-hex->string (formatter/string->padded-hex (str prefix "ゆかりさんでした" suffix)))
   )
