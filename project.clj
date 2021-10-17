1f2299569211e343abdc6d19a4ba272363cb6ba1
simana_sch <tktossi@live.com>
2021-10-17 21:00:03 +0900

    jar化する際に事前コンパイルさせるようprofile設定を追加、不要になったclojureCLR用のconfigを削除

--- project.clj
+++ project.clj
@@ -10,19 +10,8 @@
                  [org.clojure/core.async "1.3.618"]
                 ;;  [com.vladsch.flexmark/flexmark-all "0.62.2"]
                  [instaparse "1.4.10"]]
-  ;; :repositories [["com.vladsch.flexmark/flexmark-java" "https://github.com/vsch/flexmark-java"]]
   :main ^:skip-aot descript-to-video.core
   :target-path "target/%s"
-  :profiles {} 
-  :clr {:cmd-templates  {:clj-dep   [[?PATH "mono"] ["target/default/clr/clj/Debug 4.0" %1]]
-                         :clj-url   "http://sourceforge.net/projects/clojureclr/files/clojure-clr-1.4.1-Debug-4.0.zip/download"
-                         :clj-zip   "clojure-clr-1.4.1-Debug-4.0.zip"
-                         :curl      ["curl" "--insecure" "-f" "-L" "-o" %1 %2]
-                         :nuget-ver [[?PATH "mono"] [*PATH "nuget.exe"] "install" %1 "-Version" %2]
-                         :nuget-any [[?PATH "mono"] [*PATH "nuget.exe"] "install" %1]
-                         :unzip     ["unzip" "-d" %1 %2]
-                         :wget      ["wget" "--no-check-certificate" "--no-clobber" "-O" %1 %2]}
-        :deps-cmds      [[:curl  :clj-zip :clj-url]
-                         [:unzip "../clj" :clj-zip]]
-        :main-cmd      [:clj-dep "Clojure.Main.exe"]
-        :compile-cmd   [:clj-dep "Clojure.Compile.exe"]})
+  :profiles {
+             :uberjar { :aot :all}}
+  )
