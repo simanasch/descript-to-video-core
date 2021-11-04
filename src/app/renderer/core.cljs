b6569fee8255e40c0bca1693197703827b47306b
simana_sch <tktossi@live.com>
2021-11-05 01:08:07 +0900

    inputのonChangeでstateの値を更新できるようにした(だけ)

--- src/app/renderer/core.cljs
+++ src/app/renderer/core.cljs
@@ -1,10 +1,34 @@
 (ns app.renderer.core
-  (:require [reagent.core :refer [atom]]
+  (:require [reagent.core :as r]
             [reagent.dom :as rd]))
 
 (enable-console-print!)
 
-(defonce state (atom 0))
+(defonce state (r/atom 0))
+;; (defonce form-state (r/atom '{:value "test"}))
+
+(defn form-component []
+  (let [
+        other-form-state (r/atom {:value "test" :sample ""})
+        handle-change #(swap! other-form-state assoc :value %)
+        ]
+    (fn []
+      [:<>
+       [:form
+        [:div>label "テストのフォーム"]
+        [:div
+         [:label {:to "markdown-file"} "変換対象のmarkdown"]
+         [:input#markdown-file {:type "file" :title "変換対象のmarkdown" :name  "markdown-file"}]]
+        [:div
+         [:label (:sample @other-form-state)]
+         [:input {:on-change #(handle-change
+                               (.. % -target -value))}]]
+        [:button
+         {:on-click #(js/alert "click")}
+         (str "submit")]]
+      ;;  [:button {:on-click #(swap! form-state assoc :value "clicked!!!!!!")} (str "sample")]
+       [:label (:value @other-form-state)]
+       [:button {:on-click #(swap! other-form-state assoc :value "clicked!!!!!!")} (str "sample")]])))
 
 (defn root-component []
   [:div
@@ -14,7 +38,9 @@
     [:img.reagent {:src "img/reagent-logo.png"}]]
    [:button
     {:on-click #(swap! state inc)}
-    (str "Clicked " @state " times")]])
+    (str "Clicked " @state " times")]
+   [form-component]
+   ])
 
 (defn ^:dev/after-load start! []
   (rd/render
