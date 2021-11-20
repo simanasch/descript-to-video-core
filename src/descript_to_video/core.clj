34cf9b08c5c9d435db9c9f06d7b6b7f732ac95ed
simana_sch <tktossi@live.com>
2021-11-20 18:31:09 +0900

    サーバーまわりの立ち上げはcore,リクエストの処理をhandlerに移動

--- src/descript_to_video/core.clj
+++ src/descript_to_video/core.clj
@@ -4,45 +4,37 @@
   ;;  [compojure.core :refer [routes]]
   ;;  [ring.adapter.jetty :as server]
   ;; [descript-to-video.handler.main :refer [main-routes]]
-   [descript-to-video.tts.tts :as tts]
-   [descript-to-video.markdown.parser :as mdparser]
+   [org.httpkit.server :refer [run-server]]
+   [chord.http-kit :refer [wrap-websocket-handler]]
    [descript-to-video.grpc.client :as client]
-   [descript-to-video.markdown.marp :as marp]
-   [descript-to-video.util.file :as f]
-   [descript-to-video.aviutl.aviutl :as aviutl]
-   [descript-to-video.aviutl.parser :as aviutl-parser]
+   [descript-to-video.handler.websocket :as websocket-server]
   ;;  [descript-to-video.middleware :refer [wrap-dev]]
    ))
   ;; [descript-to-video.grpc.service]
   ;; (:import [io.grpc Server ServerBuilder]
   ;;          [io.grpc.stub StreamObserver]
   ;;          [descript-to-video.grpc.service GreeterServiceImpl]))
+(defonce server (atom nil))
+(def port 3000)
 
-(defn start
-  ([{:keys [markdown-path exo-path]}]
-   (let
-    [tts-lines (map mdparser/get-voiceroid-text-lines (mdparser/split-by-slides (slurp markdown-path)))
-     slides (marp/export-slides markdown-path)
-     tts-results (tts/record-lines tts-lines)
-     result-exo (-> (aviutl-parser/aviutl-object->yaml (slurp exo-path :encoding "shift-jis"))
-                    (aviutl/merge-aviutl-objects (aviutl/get-tts-objects (flatten tts-results)))
-                    (aviutl/merge-aviutl-objects (aviutl/get-slide-objects slides (aviutl/get-slide-display-positions tts-results))))]
-     (println tts-results)
-     (println slides)
-     (spit
-      (str (f/getFolderName exo-path) "\\sample_result.exo")
-      (aviutl-parser/yaml->aviutl-object result-exo) :encoding "shift-jis")
-     (str (f/getFolderName exo-path) "\\sample_result.exo")))
-  ([]
-   (start {:exo-path "./sample/sample.exo"
-           :markdown-path "./sample/sample.md"}))
-  )
+(defn start-server []
+  (reset! server 
+          (run-server (-> #'websocket-server/websocket-handler wrap-websocket-handler)
+                      {:port port})))
+
+(defn stop-server []
+  (when-not (nil? @server)
+    (@server)
+    (reset! server nil)))
+
+(defn restart-server []
+  (when @server
+    (stop-server)
+    (start-server)))
+
 
 (defn -main
   [& args]
   ;; TODO:引数の値による処理分岐
   (apply println "Received args:" args)
-  ;; (print "Now listening on port " SERVER_PORT)
-  (start args)
-  ;; mainでの呼び出しの場合は処理終了時に接続を切る
   (client/shutdown-connection!))
---
+++ src/descript_to_video/handler/websocket.clj
@@ -0,0 +1,40 @@
+(ns descript-to-video.handler.websocket
+  (:require
+   [descript-to-video.tts.tts :as tts]
+   [descript-to-video.markdown.parser :as mdparser]
+   [descript-to-video.markdown.marp :as marp]
+   [descript-to-video.util.file :as f]
+   [descript-to-video.aviutl.aviutl :as aviutl]
+   [descript-to-video.aviutl.parser :as aviutl-parser]
+
+   [clojure.core.async :refer [<! >! go]]
+   [clojure.tools.reader.edn :as edn])
+  )
+
+(defn start
+  "TODO:処理分割とリネーム
+   引数のmarkdown,exoからttsの出力とスライドの出力を行い、引数の.exoファイルにマージしたファイルのパスを返す"
+  ([{:keys [markdown-path exo-path]}]
+   (let
+    [tts-lines (map mdparser/get-voiceroid-text-lines (mdparser/split-by-slides (slurp markdown-path)))
+     slides (marp/export-slides markdown-path)
+     tts-results (tts/record-lines tts-lines)
+     result-exo (-> (aviutl-parser/aviutl-object->yaml (slurp exo-path :encoding "shift-jis"))
+                    (aviutl/merge-aviutl-objects (aviutl/get-tts-objects (flatten tts-results)))
+                    (aviutl/merge-aviutl-objects (aviutl/get-slide-objects slides (aviutl/get-slide-display-positions tts-results))))]
+     (println tts-results)
+     (println slides)
+     (spit
+      (str (f/getFolderName exo-path) "\\sample_result.exo")
+      (aviutl-parser/yaml->aviutl-object result-exo) :encoding "shift-jis")
+     (str (f/getFolderName exo-path) "\\sample_result.exo")))
+  ([]
+   (start {:exo-path "./sample/sample.exo"
+           :markdown-path "./sample/sample.md"}))
+  )
+
+(defn websocket-handler [{:keys [ws-channel] :as req}]
+  (go (let [{:keys [message]} (<! ws-channel)]
+        (println "Received message:" message)
+        (>! ws-channel "hello client from server!")
+        (>! ws-channel (start (edn/read-string message))))))
